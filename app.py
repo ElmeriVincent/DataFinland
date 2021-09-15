7c29a06a67eb6bafbbecc2c102f91cbea55c4382
ElmeriVincent <77973084+ElmeriVincent@users.noreply.github.com>
2021-09-15 21:29:38 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.markdown("<h1 style='text-align: center;'>Finland Now!</h1>", unsafe_allow_html=True)
