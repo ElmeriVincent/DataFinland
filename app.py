a987c26a248625ac84ef6281a7ced9fefdf4c217
Elmeri Keitaanranta <you@example.com>
2021-09-16 22:08:56 +0300

    first commit

--- app.py
+++ app.py
@@ -2,5 +2,24 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+import requests
+import json
+from PIL import Image
 
-st.markdown("<h1 style='text-align: center;'>Finland Now!</h1>", unsafe_allow_html=True)
+st.set_page_config(page_title="Data Finland")
+st.markdown("<h1 style='text-align: center;'>Data Finland!</h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: left;'>This is a small data app about Finland,which include roadaccidents, population, most populated areas, etc...</p>", unsafe_allow_html=True)
+
+
+
+
+### --- LOAD DATAFRAME
+excel_file = 'data.xlsx'
+
+df = pd.read_excel(excel_file,
+                    usecols='A:B',
+                    header=0)
+
+st.dataframe(df)
+
