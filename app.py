6190107b3d11a268c3a255d8aab14ab021d7aa1a
Elmeri Keitaanranta <you@example.com>
2021-09-17 22:38:42 +0300

    raw data button

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from pandas.io import excel
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -6,20 +7,28 @@ import time
 import requests
 import json
 from PIL import Image
+import plotly.express as px
 
+
+#set_page_config sets a title on your window.
 st.set_page_config(page_title="Data Finland")
+
+
 st.markdown("<h1 style='text-align: center;'>Data Finland!</h1>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: left;'>This is a small data app about Finland,which include roadaccidents, population, most populated areas, etc...</p>", unsafe_allow_html=True)
-
-
 
 
 ### --- LOAD DATAFRAME
 excel_file = 'data.xlsx'
 
-df = pd.read_excel(excel_file,
+data = pd.read_excel(excel_file,
                     usecols='A:B',
                     header=0)
 
-st.dataframe(df)
+#st.dataframe(df)
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
 
