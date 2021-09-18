a229a824729002419018eff6285cb398a4e8584a
Elmeri Keitaanranta <ElmeriVincent@gmail.com>
2021-09-19 00:24:00 +0300

    new visual of population

--- app.py
+++ app.py
@@ -4,31 +4,29 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-import requests
-import json
 from PIL import Image
-import plotly.express as px
-
+import altair as aa
 
 #set_page_config sets a title on your window.
 st.set_page_config(page_title="Data Finland")
 
 
-st.markdown("<h1 style='text-align: center;'>Data Finland!</h1>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: left;'>This is a small data app about Finland,which include roadaccidents, population, most populated areas, etc...</p>", unsafe_allow_html=True)
+#heading
+st.markdown("<h1 style='text-align: center;  { font-family: finlandica; } '>Data Finland!</h1><br>", unsafe_allow_html=True)
 
 
 ### --- LOAD DATAFRAME
-excel_file = 'data.xlsx'
+excel_file = "data.xlsx"
 
 data = pd.read_excel(excel_file,
-                    usecols='A:B',
-                    header=0)
+                usecols='A:B',
+                parse_dates=True)
 
-#st.dataframe(df)
-
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+data.set_index('Year', inplace=True)
+st.line_chart(data)
 
 
+#raw data option
+if st.checkbox("Show raw data"):
+        st.subheader("Raw data")
+        st.write(data)
