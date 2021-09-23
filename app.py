9445dca41b284b936d54b06946f8c3eeca8b4663
Elmeri Keitaanranta <ElmeriVincent@gmail.com>
2021-09-23 22:09:08 +0300

    new look.

--- app.py
+++ app.py
@@ -9,11 +9,26 @@ import altair as aa
 
 
 #set_page_config sets a title on your window.
-st.set_page_config(page_title="Data Finland")
+st.set_page_config(page_title="Data Finland", layout="centered",
+                                page_icon="🧊",
+                                initial_sidebar_state="expanded")
+
+
+st.write("![Twitter Follow](https://img.shields.io/twitter/follow/ElmeriVincent?style=social)")
 
 
 #heading
 st.markdown("<h1 style='text-align: center;  { font-family: finlandica; } '>Data Finland!</h1><br>", unsafe_allow_html=True)
+
+
+'''**Population growth in Finland 1960-2020**'''
+
+
+def local_css(file_name):
+        with open(file_name) as f:
+                st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+
+local_css("style.css")
 
 
 ### --- LOAD DATAFRAME
@@ -27,6 +42,10 @@ data.set_index('Year', inplace=True)
 
 st.line_chart(data)
 
+
+selected_indices = st.multiselect('Select the Specific year.', data.index)
+selected_rows = data.loc[selected_indices]
+st.write('### Selected Rows', selected_rows)
 
 #raw data option
 if st.checkbox("Show raw data"):
